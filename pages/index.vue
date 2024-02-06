b956db4d24b6e2500597ef2b21d5067a29c737cd
esmaydogdu <esmaydogdu@gmail.com>
2024-02-06 23:34:18 +0300

    init blend call

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,15 @@
     <h1>welcome {{ userProfile.display_name }}</h1>
     <input v-model="searchQuery" @input="search" placeholder="Search for songs...">
     <ul>
-      <li v-for="track in searchResults" :key="track.id">{{ track.name }} by {{ track.artists[0].name }}</li>
+      <li v-for="track in searchResults" :key="track.id" @click="addTrack(track)">{{ track.name }} by {{
+        track.artists[0].name }}
+      </li>
     </ul>
+    <div>
+      {{ selectedTracks }}
+    </div>
     <button @click="logout">Logout</button>
+    <button @click="createBlend">Blend</button>
   </div>
 </template>
 
@@ -17,6 +23,7 @@ export default {
     return {
       searchQuery: '',
       searchResults: [],
+      selectedTracks: []
     };
   },
   async asyncData({ $auth, $axios }) {
@@ -47,6 +54,24 @@ export default {
         console.error('Logout error:', error);
 
       }
+    },
+    addTrack(track) {
+      this.selectedTracks.push(track)
+    },
+    async createBlend() {
+      const id1 = this.selectedTracks[0].id
+      const id2 = this.selectedTracks[1].id
+      console.log('ids', id1, id2)
+      const response = await this.$axios.get(
+        `https://api.spotify.com/v1/recommendations?seed_tracks=${id1},${id2}`,
+        {
+          headers: {
+            Authorization: `Bearer ${this.$auth.strategy.token}`,
+          },
+        }
+      );
+      console.log('recomm', response.data)
+      return response.data;
     },
     search: debounce(function () {
       try {
